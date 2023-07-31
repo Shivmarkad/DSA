4fe7c84d4bffa57bc6ec0b82075e01564f3f8e39
Shivganesh <shivmarkad99@gmail.com>
2023-07-31 23:44:10 +0530

    [Shivganesh] Learning DSA

---
+++ 01_array.js
@@ -0,0 +1,15 @@
+//Array traversal and accessing 
+
+let data = [1, 4, 5,6,8,222];
+
+for(let i= 0;i<data.length;i++){
+    console.log(`This is the elemeny ${data[i]} at ${i}`);
+}
+
+//accessing at perticular index
+
+let ind = 3;
+
+console.log(`Element at index ${ind} is ${data[ind]}`)
+
+
