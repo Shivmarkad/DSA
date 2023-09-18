b706dd60766b7658fa31cf25dff0ee97f5e04ad0
Shivganesh <shivmarkad99@gmail.com>
2023-09-18 11:59:50 +0530

    [Shivganesh] Added: AddNode method to add new node in the tree

--- tree/newTree.js
+++ tree/newTree.js
@@ -5,3 +5,43 @@ class Node{
         this.right = right
     }
 }
+
+class BST{
+    constructor(data=null){
+        this.root = new Node(data)
+    }
+
+    addNode(data){
+        const newNode = new Node(data)
+        if(!this.root){
+            this.root = newNode
+        }
+        let node = this.root;
+        function searchTree(node){
+            if(data < node.data){
+                if(!node.left){
+                    node.left = newNode
+                    return;
+                }
+                return searchTree(node.left)
+            }
+            else if(data > node.data){
+                if(!node.right){
+                    node.right = newNode
+                    return;
+                }
+                return searchTree(node.right)
+            }else{
+                return;
+            }
+        }
+        return searchTree(node)
+    }
+}
+
+const tree = new BST(12)
+
+tree.addNode(1)
+tree.addNode(6)
+tree.addNode(15)
+console.log(tree)
