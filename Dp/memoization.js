68c06d0e16144c68e369e9a8a1cb5c747b17934a
Shivganesh <shivmarkad99@gmail.com>
2023-09-21 14:42:09 +0530

    [Shivganesh] Added: fibonacci series using memoization

--- Dp/memoization.js
+++ Dp/memoization.js
@@ -7,4 +7,15 @@ function fiboNacci(n){
     return fiboNacci(n-1) + fiboNacci(n-2)
 }
 
+function fibonacciVal(n) {
+    const memo = []
+    memo[0] = 0
+    memo[1] = 1
+    for (let i = 2; i <= n; i++) {
+        memo[i] = memo[i - 1] + memo[i - 2]
+    }
+    return memo[n]
+}
+
+console.log(fibonacciVal(7))
 console.log(fiboNacci(7))
