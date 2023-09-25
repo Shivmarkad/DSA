95b570e1faf7e26c663601edbd1486719c0a1890
Shivganesh <shivmarkad99@gmail.com>
2023-09-25 13:14:32 +0530

    [Shivganesh] Added: using memoization to store fibonacci series and return already calculated fibonacci number

--- Dp/memoization.js
+++ Dp/memoization.js
@@ -1,21 +1,27 @@
-function fiboNacci(n){
-    if(n == 0){
-        return 0
-    }else if(n == 1){
-        return 1
-    }
-    return fiboNacci(n-1) + fiboNacci(n-2)
-}
+// function fiboNacci(n) {
+//     if (n == 0) {
+//         return 0
+//     } else if (n == 1) {
+//         return 1
+//     }
+//     return fiboNacci(n - 1) + fiboNacci(n - 2)
+// }
+
+const memo = []
+memo[0] = 0
+memo[1] = 1
 
 function fibonacciVal(n) {
-    const memo = []
-    memo[0] = 0
-    memo[1] = 1
+    if (memo.length > n) {
+        return memo[n]
+    }
     for (let i = 2; i <= n; i++) {
         memo[i] = memo[i - 1] + memo[i - 2]
     }
     return memo[n]
 }
 
-console.log(fibonacciVal(7))
-console.log(fiboNacci(7))
+console.log(fibonacciVal(9))
+console.log(fibonacciVal(6))
+// console.log(fiboNacci(7))
+console.log(memo)
